95baff2cd3871890e269220564f50f44159ba84a
nrndda <nrndda@gmail.com>
2013-06-30 23:24:54 +0400

    Some changes into crda_set.sh
    Signed-off-by: nrndda <nrndda@gmail.com>

--- net-wireless/crda/files/crda_set.sh
+++ net-wireless/crda/files/crda_set.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+#CURRENT_COUNTRY=`iw reg get | grep -i country | cut -d ' ' -f 2 | cut -b 1-2`
+#COUNTRY=`iw reg get | grep -i country | cut -d ' ' -f 2 | cut -b 1-2` /sbin/crda
 COUNTRY=00 /sbin/crda
