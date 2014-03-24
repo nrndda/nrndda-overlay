1c14ac288ab96bb64d17a5753b12ca1f5002f2d2
nrndda <nrndda@gmail.com>
2014-03-24 18:25:30 +0400

    Revert country code in crda_sh to 00. It must be that!
    Signed-off-by: nrndda <nrndda@gmail.com>

--- sys-apps/my-systemd-units/files/crda_set.sh
+++ sys-apps/my-systemd-units/files/crda_set.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #CURRENT_COUNTRY=`iw reg get | grep -i country | cut -d ' ' -f 2 | cut -b 1-2`
 #COUNTRY=`iw reg get | grep -i country | cut -d ' ' -f 2 | cut -b 1-2` /sbin/crda
-COUNTRY=RU /usr/sbin/crda
+#It MUST be 00 code. After that it'll swich to needed country code.
+COUNTRY=00 /usr/sbin/crda
