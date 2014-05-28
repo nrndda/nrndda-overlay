b0aa00277e93e6fbab69e1ba9b9c062d08c6886b
nrndda <nrndda@gmail.com>
2014-05-28 17:20:30 +0400

    Silence crda_set.sh
    Signed-off-by: nrndda <nrndda@gmail.com>

--- sys-apps/my-systemd-units/files/crda_set.sh
+++ sys-apps/my-systemd-units/files/crda_set.sh
@@ -2,4 +2,4 @@
 #CURRENT_COUNTRY=`iw reg get | grep -i country | cut -d ' ' -f 2 | cut -b 1-2`
 #COUNTRY=`iw reg get | grep -i country | cut -d ' ' -f 2 | cut -b 1-2` /sbin/crda
 #It MUST be 00 code. After that it'll swich to needed country code.
-COUNTRY=00 /usr/sbin/crda
+COUNTRY=00 /usr/sbin/crda 2>/dev/null
