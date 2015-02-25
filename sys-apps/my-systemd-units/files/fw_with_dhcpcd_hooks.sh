d9dc5f75efa1e7f7895ea0fd7fcf136b2bebd950
nrndda <nrndda@gmail.com>
2015-02-25 21:58:39 +0300

    Run firewall script only when new lease received.
    Signed-off-by: nrndda <nrndda@gmail.com>

--- sys-apps/my-systemd-units/files/fw_with_dhcpcd_hooks.sh
+++ sys-apps/my-systemd-units/files/fw_with_dhcpcd_hooks.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-/lib/dhcpcd/dhcpcd-run-hooks
-
+#BOUND|BOUND6|REBIND|REBIND6|RENEW|RENEW6)
 case "$reason" in
-BOUND|BOUND6|REBIND|REBIND6|RENEW|RENEW6)
-/usr/local/sbin/fw_full.sh
-/etc/miniupnpd/iptables_init.sh
-/etc/miniupnpd/ip6tables_init.sh
-;;
+	BOUND|BOUND6)
+		/usr/local/sbin/fw_full.sh
+		/etc/miniupnpd/iptables_init.sh
+		/etc/miniupnpd/ip6tables_init.sh;;
 esac
 
-
+/lib/dhcpcd/dhcpcd-run-hooks
