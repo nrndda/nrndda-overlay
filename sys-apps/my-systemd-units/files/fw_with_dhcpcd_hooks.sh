e662277eab7dff312bc2de98f62a12d1955fb102
nrndda <nrndda@gmail.com>
2015-02-26 01:06:11 +0300

    Run firewall rules each time and after dhcpcd hooks.
    Signed-off-by: nrndda <nrndda@gmail.com>

--- sys-apps/my-systemd-units/files/fw_with_dhcpcd_hooks.sh
+++ sys-apps/my-systemd-units/files/fw_with_dhcpcd_hooks.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-#BOUND|BOUND6|REBIND|REBIND6|RENEW|RENEW6)
+/lib/dhcpcd/dhcpcd-run-hooks
+
 case "$reason" in
-	BOUND|BOUND6)
+	BOUND|BOUND6|REBIND|REBIND6|RENEW|RENEW6)
 		/usr/local/sbin/fw_full.sh
 		/etc/miniupnpd/iptables_init.sh
 		/etc/miniupnpd/ip6tables_init.sh;;
 esac
-
-/lib/dhcpcd/dhcpcd-run-hooks
