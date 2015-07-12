ada5a894c285dc5f01b74cbe799d9e49fb2b29b0
nrndda <nrndda@gmail.com>
2015-07-12 14:38:10 +0300

    Add additional reasons into dhcpcd hooks.
    Signed-off-by: nrndda <nrndda@gmail.com>

--- sys-apps/my-systemd-units/files/99-dhcpcd_fw_hook.sh
+++ sys-apps/my-systemd-units/files/99-dhcpcd_fw_hook.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 case "$reason" in
-        BOUND|BOUND6|REBIND|REBIND6|RENEW|RENEW6)
-                /usr/local/sbin/fw_full_with_ip.sh
+        BOUND|BOUND6|REBIND|REBIND6|RENEW|RENEW6|REBOOT|REBOOT6|INFORM|INFORM6)
+                /usr/local/sbin/fw_full.sh
                 /etc/miniupnpd/iptables_init.sh
                 /etc/miniupnpd/ip6tables_init.sh
 	;;
